bcf8d724f72a6926fb7d5e96ec5763cff7a99370
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-03-31 15:09:31 +0200

    fixed the last fix :P

---
+++ lib/model.rb
@@ -0,0 +1,32 @@
+require 'barby'
+require 'barby/outputter/png_outputter'
+
+require 'dm-core'
+DataMapper.setup(:default, 'sqlite::memory:')
+
+class Ticket
+  DURATION = 3600
+  include DataMapper::Resource
+
+  property :id         , Serial
+  property :created_at , Time
+  property :abus_code  , String, :unique => true
+  property :atram_code , String, :unique => true
+  
+  def expired?
+    Time.now - created_at > DURATION
+  end
+  
+  def to_barcode
+    Barby::QrCode.new("#{abus_code}_#{atram_code}").to_png
+  end
+
+  def self.retrieve(abus_code, atram_code)
+    Ticket.first :abus_code => abus_code, :atram_code => atram_code
+  end
+end
+
+DataMapper.finalize
+
+require  'dm-migrations'
+DataMapper.auto_migrate!
